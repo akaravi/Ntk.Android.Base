d7d850ff18205e42d3816ef5ab9292d0c51da6ae
Alireza Karavi <karavi.alireza@gmail.com>
2019-09-03 14:04:15 +0430

    Change About Model

--- api/src/main/java/ntk/base/api/core/model/CoreAboutUsResponse.java
+++ api/src/main/java/ntk/base/api/core/model/CoreAboutUsResponse.java
@@ -8,42 +8,5 @@ import ntk.base.api.model.ErrorException;
 public class CoreAboutUsResponse extends ErrorException {
 
     @SerializedName("Item")
-    public About Item;
-}
-class About {
-    @SerializedName("Id")
-    public long Id;
-
-    @SerializedName("WebUrl")
-    public String WebUrl;
-
-    @SerializedName("LogoUrl")
-    public String LogoUrl;
-
-    @SerializedName("Content")
-    public String Content;
-
-    @SerializedName("Email")
-    public String Email;
-
-    @SerializedName("Instagram")
-    public String Instagram;
-
-    @SerializedName("Telegram")
-    public String Telegram;
-
-    @SerializedName("Tel1")
-    public String Tel1;
-
-    @SerializedName("Tel2")
-    public String Tel2;
-
-    @SerializedName("OfficeNo")
-    public String OfficeNo;
-
-    @SerializedName("MobileNo")
-    public String MobileNo;
-
-    @SerializedName("Address")
-    public String Address;
+    public CoreAbout Item;
 }
