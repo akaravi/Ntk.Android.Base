6adae1a3864fa5e6c8f6ac793a231b56fa493ca0
Alireza Karavi <karavi.alireza@gmail.com>
2019-09-07 12:10:44 +0430

    add Title to Aboutus Model

--- api/src/main/java/ntk/base/api/core/model/CoreAbout.java
+++ api/src/main/java/ntk/base/api/core/model/CoreAbout.java
@@ -6,36 +6,104 @@ public class CoreAbout {
     @SerializedName("Id")
     public long Id;
 
+    @SerializedName("TitleWebUrl")
+    public String TitleWebUrl;
+
     @SerializedName("WebUrl")
     public String WebUrl;
+
+
+    @SerializedName("TitleLogoUrl")
+    public String TitleLogoUrl;
 
     @SerializedName("LogoUrl")
     public String LogoUrl;
 
+    @SerializedName("TitleContent")
+    public String TitleContent;
+
     @SerializedName("Content")
     public String Content;
+
+    @SerializedName("TitleEmail")
+    public String TitleEmail;
 
     @SerializedName("Email")
     public String Email;
 
+    @SerializedName("TitleInstagram")
+    public String TitleInstagram;
+
     @SerializedName("Instagram")
     public String Instagram;
+
+    @SerializedName("TitleTelegram")
+    public String TitleTelegram;
 
     @SerializedName("Telegram")
     public String Telegram;
 
+
+    @SerializedName("TitleTel1")
+    public String TitleTel1;
+
     @SerializedName("Tel1")
     public String Tel1;
+
+    @SerializedName("TitleTel2")
+    public String TitleTel2;
 
     @SerializedName("Tel2")
     public String Tel2;
 
+
+    @SerializedName("TitleOfficeNo")
+    public String TitleOfficeNo;
+
     @SerializedName("OfficeNo")
     public String OfficeNo;
+
+    @SerializedName("TitleMobileNo")
+    public String TitleMobileNo;
+
 
     @SerializedName("MobileNo")
     public String MobileNo;
 
+    @SerializedName("TitleAddress")
+    public String TitleAddress;
+
+
     @SerializedName("Address")
     public String Address;
+
+
+    @SerializedName("TitleFaceBook")
+    public String TitleFaceBook;
+
+
+    @SerializedName("FaceBook")
+    public String FaceBook;
+
+    @SerializedName("TitleTwitter")
+    public String TitleTwitter;
+
+
+    @SerializedName("Twitter")
+    public String Twitter;
+
+    @SerializedName("TitlePlayStore")
+    public String TitlePlayStore;
+
+
+    @SerializedName("PlayStore")
+    public String PlayStore;
+
+    @SerializedName("TitleYoutube")
+    public String TitleYoutube;
+
+
+    @SerializedName("Youtube")
+    public String Youtube;
+
 }
