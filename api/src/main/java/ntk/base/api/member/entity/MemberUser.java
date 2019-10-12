4c2a96e60945b53092ae8a2d875bc09699b0186f
Alireza Karavi <karavi.alireza@gmail.com>
2019-10-12 10:24:47 +0330

    member user mode complet

--- api/src/main/java/ntk/base/api/member/entity/MemberUser.java
+++ api/src/main/java/ntk/base/api/member/entity/MemberUser.java
@@ -13,40 +13,107 @@ public class MemberUser extends BaseEntity {
     @SerializedName("LinkCmsUserId")
     public Long LinkCmsUserId;
 
-    @SerializedName("Title")
-    public String Title;
-
-    @SerializedName("Description")
-    public String Description;
+    @SerializedName("LinkDeviceId")
+    public String LinkDeviceId;
 
     @SerializedName("JoinId")
     public String JoinId;
 
+    @SerializedName("Gender")
+    public Long Gender;
+
+    @SerializedName("FirstName")
+    public String FirstName;
+
+    @SerializedName("LastName")
+    public String LastName;
+
     @SerializedName("LinkLocationId")
     public Long LinkLocationId;
-    /// <summary>
-    /// موقعیت جغرافیایی
-    /// </summary>
+
+
+    @SerializedName("DateBirth")
+    public String DateBirth;
+
+    @SerializedName("BirthPlace")
+    public String BirthPlace;
+
+    @SerializedName("NationalCode")
+    public String NationalCode;
+
+    @SerializedName("Email")
+    public String Email;
+
+    @SerializedName("MobileNo")
+    public String MobileNo;
+
+
+    @SerializedName("PhoneNo")
+    public String PhoneNo;
+
+    @SerializedName("OfficeNo")
+    public String OfficeNo;
+
+
+    @SerializedName("Address")
+    public String Address;
+
+
+    @SerializedName("LinkMainImageId")
+    public Long LinkMainImageId;
+
+    @SerializedName("LinkFileIds")
+    public String LinkFileIds;
+
+
+
     @SerializedName("Geolocationlatitude")
     public Double Geolocationlatitude;
 
     @SerializedName("Geolocationlongitude")
     public Double Geolocationlongitude;
 
-    @SerializedName("DateManufacture")
-    public String DateManufacture;
 
-    @SerializedName("PlaceProduction")
-    public String PlaceProduction;
 
-    @SerializedName("SerialRfId")
-    public String SerialRfId;
+    @SerializedName("InstagramId")
+    public String InstagramId;
 
-    @SerializedName("SerialNumber")
-    public String SerialNumber;
+    @SerializedName("TelegramId")
+    public String TelegramId;
 
-    @SerializedName("Address")
-    public String Address;
+    @SerializedName("OtherInfo1")
+    public String OtherInfo1;
+
+    @SerializedName("OtherInfo2")
+    public String OtherInf2;
+
+    @SerializedName("OtherInfo3")
+    public String OtherInfo3;
+
+    @SerializedName("OtherInfo4")
+    public String OtherInfo4;
+
+    @SerializedName("OtherInfo5")
+    public String OtherInfo5;
+
+    @SerializedName("OtherInfo6")
+    public String OtherInfo6;
+
+    @SerializedName("OtherInfo7")
+    public String OtherInfo7;
+
+    @SerializedName("OtherInfo8")
+    public String OtherInfo8;
+
+    @SerializedName("OtherInfo9")
+    public String OtherInfo9;
+
+    @SerializedName("OtherInfo10")
+    public String OtherInfo10;
+
+
+
+
 
     @SerializedName("MemberUserGroup")
     public List<MemberUserGroup> MemberUserGroup;
