0f002d577d309c957d2bce2c28ee5c6f47879378
ixiflutter1313 <mj.parishani@outlook.com>
2020-10-31 14:51:38 +0330

    Update FileUploadModel.java

--- api/src/main/java/ntk/base/api/file/entity/FileUploadModel.java
+++ api/src/main/java/ntk/base/api/file/entity/FileUploadModel.java
@@ -3,21 +3,22 @@ package ntk.base.api.file.entity;
 import com.google.gson.annotations.SerializedName;
 
 public class FileUploadModel {
-
+    @SerializedName("FileName")
+    public String FileName;
+    @SerializedName("FileKey")
+    public String FileKey;
     @SerializedName("ChunkNumber")
     public int ChunkNumber;
     @SerializedName("ChunkSize")
-    public long ChunkSize ;
+    public long ChunkSize;
     @SerializedName("CurrentChunkSize")
     public long CurrentChunkSize;
     @SerializedName("TotalSize")
-    public long TotalSize ;
+    public long TotalSize;
     @SerializedName("Identifier")
-    public String Identifier ;
-    @SerializedName("FileName")
-    public String FileName ;
+    public String Identifier;
     @SerializedName("RelativePath")
-    public String RelativePath ;
+    public String RelativePath;
     @SerializedName("TotalChunks")
-    public int TotalChunks ;
+    public int TotalChunks;
 }
