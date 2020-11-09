c808cf2fd3db48b647895ed82cae6288b83f4ab0
ixiflutter1313 <mj.parishani@outlook.com>
2020-11-09 18:05:26 +0330

    add static feature

---
+++ api/src/main/java/ntk/android/base/ApplicationStaticParameter.java
@@ -0,0 +1,8 @@
+package ntk.android.base;
+
+public class ApplicationStaticParameter {
+    public static String URL ;
+    public static String TONEN ;
+    public static String DEVICE_TOKEN ;
+
+}
