9ca03525e49394689be2f7198e836211fd20dd21
ixiflutter1313 <mj.parishani@outlook.com>
2020-11-26 13:32:42 +0330

    Update Extras.java

--- api/src/main/java/ntk/android/base/Extras.java
+++ api/src/main/java/ntk/android/base/Extras.java
@@ -1,5 +1,6 @@
 package ntk.android.base;
 
 public class Extras {
-    public static final String EXTRA_FIRST_ARG ="";
+    public static final String EXTRA_FIRST_ARG ="fir st_arg";
+    public static final String EXTRA_SECOND_ARG ="Second_arg";
 }
