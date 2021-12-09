c4310f1cb2cca7252e0eb9b25350d2bf6fd466ea
work-Idev <parishani@IMT-ISF.local>
2021-12-09 12:11:47 +0330

    Create SearchTypeModel.java

---
+++ api/src/main/java/ntk/android/base/entitymodel/base/SearchTypeModel.java
@@ -0,0 +1,11 @@
+package ntk.android.base.entitymodel.base;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SearchTypeModel {
+    @SerializedName("SortType")
+    public int SortType;
+
+    @SerializedName("SortColumn")
+    public String SortColumn;
+}
