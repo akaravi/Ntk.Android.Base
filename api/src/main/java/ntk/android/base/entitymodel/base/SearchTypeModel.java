3a18151fb70ef91cbc1a39265a458b1f0d70020f
ixiflutter1313 <mj.parishani@outlook.com>
2021-12-10 21:59:54 +0330

    Update SearchTypeModel.java

--- api/src/main/java/ntk/android/base/entitymodel/base/SearchTypeModel.java
+++ api/src/main/java/ntk/android/base/entitymodel/base/SearchTypeModel.java
@@ -3,9 +3,38 @@ package ntk.android.base.entitymodel.base;
 import com.google.gson.annotations.SerializedName;
 
 public class SearchTypeModel {
-    @SerializedName("SortType")
+    public static int SORT_DESCENDING = 0;
+    public static int SORT_ASCENDING = 1;
+    public static int SORT_RANDOM = 2;
     public int SortType;
 
-    @SerializedName("SortColumn")
     public String SortColumn;
+    public String DisplayName;
+
+    public int getSortType() {
+        return SortType;
+    }
+
+    public SearchTypeModel setSortType(int sortType) {
+        SortType = sortType;
+        return this;
+    }
+
+    public String getSortColumn() {
+        return SortColumn;
+    }
+
+    public SearchTypeModel setSortColumn(String sortColumn) {
+        SortColumn = sortColumn;
+        return this;
+    }
+
+    public String getDisplayName() {
+        return DisplayName;
+    }
+
+    public SearchTypeModel setDisplayName(String displayName) {
+        DisplayName = displayName;
+        return this;
+    }
 }
