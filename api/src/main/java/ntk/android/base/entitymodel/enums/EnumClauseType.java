3b9cc08ee22c218278676dded86322b3f51a78d9
work-Idev <parishani@IMT-ISF.local>
2021-12-23 11:40:04 +0330

    add enum

--- api/src/main/java/ntk/android/base/entitymodel/enums/EnumClauseType.java
+++ api/src/main/java/ntk/android/base/entitymodel/enums/EnumClauseType.java
@@ -1,4 +1,16 @@
 package ntk.android.base.entitymodel.enums;
 
 public enum  EnumClauseType {
+    Or ( 1),
+    And ( 2);
+
+    private final int index;
+
+    EnumClauseType(int index) {
+        this.index = index;
+    }
+
+    public int index() {
+        return index;
+    }
 }
