3adccad78070a4da999f209d564f662a67d29621
work-Idev <parishani@IMT-ISF.local>
2021-12-29 10:28:54 +0330

    Create FromToClass.java

---
+++ api/src/main/java/ntk/android/base/appclass/FromToClass.java
@@ -0,0 +1,14 @@
+package ntk.android.base.appclass;
+
+public class FromToClass {
+    Object from;
+    Object to;
+
+    public void setFrom(Object f) {
+        from = f;
+    }
+
+    public void setTo(Object f) {
+        to = f;
+    }
+}
