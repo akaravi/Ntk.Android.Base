bf52c290781b578cb189869db8de06c201f9b623
MJ.Parishani devs <56607419+idindevelop@users.noreply.github.com>
2022-03-09 10:11:24 +0330

    Update EstatePropertyService.java

--- api/src/main/java/ntk/android/base/services/estate/EstatePropertyService.java
+++ api/src/main/java/ntk/android/base/services/estate/EstatePropertyService.java
@@ -3,12 +3,15 @@ package ntk.android.base.services.estate;
 import android.content.Context;
 
 import io.reactivex.Observable;
+import ntk.android.base.dtomodel.core.CoreModuleReportAbuseDtoModel;
 import ntk.android.base.entitymodel.base.ErrorException;
 import ntk.android.base.entitymodel.base.ErrorExceptionBase;
 import ntk.android.base.entitymodel.base.FilterModel;
 import ntk.android.base.entitymodel.estate.EstatePropertyModel;
+import ntk.android.base.entitymodel.news.NewsCommentModel;
 import ntk.android.base.services.base.CmsApiFavoriteBase;
 import ntk.android.base.services.base.CmsApiServerBase;
+import ntk.android.base.services.base.CmsReportAbuse;
 
 public class EstatePropertyService extends CmsApiServerBase<EstatePropertyModel, String> {
     public EstatePropertyService(Context context) {
@@ -25,5 +28,10 @@ public class EstatePropertyService extends CmsApiServerBase<EstatePropertyModel,
 
     public Observable<ErrorException<EstatePropertyModel>> getFavoriteList(FilterModel request) {
         return new CmsApiFavoriteBase<EstatePropertyModel, String>(context, "EstateProperty", EstatePropertyModel.class).getFavoriteList(request);
+    }
+
+    public Observable<ErrorException<EstatePropertyModel>> report(CoreModuleReportAbuseDtoModel model) {
+        return new CmsReportAbuse<EstatePropertyModel,String>(context, "EstateProperty", EstatePropertyModel.class).addReport(model);
+
     }
 }
