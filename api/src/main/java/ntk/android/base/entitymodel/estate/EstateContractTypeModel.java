6d3573d332398c2d259fa06a856adff6e836ce63
ixiflutter1313 <mj.parishani@outlook.com>
2022-07-29 11:28:26 +0430

    Update EstateContractTypeModel.java

--- api/src/main/java/ntk/android/base/entitymodel/estate/EstateContractTypeModel.java
+++ api/src/main/java/ntk/android/base/entitymodel/estate/EstateContractTypeModel.java
@@ -7,6 +7,18 @@ import ntk.android.base.entitymodel.base.BaseModuleEntity;
 public class EstateContractTypeModel extends BaseModuleEntity<String> {
     @SerializedName("Title")
     public String Title;
+    @SerializedName("TitleML")
+    public String TitleML;
+    @SerializedName("TitleResourceLanguage")
+    public String TitleResourceLanguage;
+    @SerializedName("ContentCount")
+    public int ContentCount;
+    @SerializedName("TitleSoldIt")
+    public String TitleSoldIt;
+    @SerializedName("TitleSoldItML")
+    public String TitleSoldItML;
+    @SerializedName("TitleSoldItResourceLanguage")
+    public String TitleSoldItResourceLanguage;
     @SerializedName("Description")
     public String Description;
     @SerializedName("HasSalePrice")
@@ -15,39 +27,59 @@ public class EstateContractTypeModel extends BaseModuleEntity<String> {
     public boolean HasRentPrice;
     @SerializedName("HasDepositPrice")
     public boolean HasDepositPrice;
-    @SerializedName("UnitSalePrice")
-    public String UnitSalePrice;
-
-    @SerializedName("UnitRentPrice")
-    public String UnitRentPrice;
-    @SerializedName("UnitDepositPrice")
-    public String UnitDepositPrice;
-
-    @SerializedName("SalePriceAllowAgreement")
-    public boolean SalePriceAllowAgreement;
-    @SerializedName("SalePriceMin")
-    public Long SalePriceMin;
-    @SerializedName("SalePriceMax")
-    public Long SalePriceMax;
-
-    @SerializedName("RentPriceAllowAgreement")
-    public boolean RentPriceAllowAgreement;
-    @SerializedName("RentPriceMin")
-    public Long RentPriceMin;
-    @SerializedName("RentPriceMax")
-    public Long RentPriceMax;
-
-    @SerializedName("DepositPriceAllowAgreement")
-    public boolean DepositPriceAllowAgreement;
-    @SerializedName("DepositPriceMin")
-    public Long DepositPriceMin;
-    @SerializedName("DepositPriceMax")
-    public Long DepositPriceMax;
+    @SerializedName("HasPeriodPrice")
+    public boolean HasPeriodPrice;
 
     @SerializedName("TitleSalePrice")
     public String TitleSalePrice;
-    @SerializedName("TitleDepositPrice")
-    public String TitleDepositPrice;
+    @SerializedName("TitleSalePriceML")
+    public String TitleSalePriceML;
+    @SerializedName("TitleSalePriceResourceLanguage")
+    public String TitleSalePriceResourceLanguage;
+
     @SerializedName("TitleRentPrice")
     public String TitleRentPrice;
+    @SerializedName("TitleRentPriceML")
+    public String TitleRentPriceML;
+    @SerializedName("TitleRentPriceResourceLanguage")
+    public String TitleRentPriceResourceLanguage;
+
+    @SerializedName("TitleDepositPrice")
+    public String TitleDepositPrice;
+     @SerializedName("TitleDepositPriceML")
+    public String TitleDepositPriceML;
+     @SerializedName("titleDepositPriceResourceLanguage")
+    public String TitleDepositPriceResourceLanguage;
+
+   @SerializedName("TitlePeriodPrice")
+    public String TitlePeriodPrice;
+     @SerializedName("TitlePeriodPriceML")
+    public String TitlePeriodPriceML;
+     @SerializedName("TitlePeriodPriceResourceLanguage")
+    public String TitlePeriodPriceResourceLanguage;
+
+
+    @SerializedName("SalePriceAllowAgreement")
+    public boolean SalePriceAllowAgreement;
+
+
+    @SerializedName("RentPriceAllowAgreement")
+    public boolean RentPriceAllowAgreement;
+    @SerializedName("RentPriceAllowPriodCount")
+    public boolean RentPriceAllowPriodCount;
+    @SerializedName("RentPricePriodTitle")
+    public String RentPricePriodTitle;
+    @SerializedName("RentPricePriodTitleML")
+   public String RentPricePriodTitleML;
+    @SerializedName("RentPricePriodTitleResourceLanguage")
+   public String RentPricePriodTitleResourceLanguage;
+
+    @SerializedName("DepositPriceAllowAgreement")
+    public boolean DepositPriceAllowAgreement;
+    @SerializedName("PeriodPriceAllowAgreement")
+    public boolean PeriodPriceAllowAgreement;
+
+
+
+
 }
